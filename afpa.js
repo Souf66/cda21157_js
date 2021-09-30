fb9711cc5a4aca9b2b4ca3885948637fc8f300c6
Moussa afpa <>
2021-09-30 17:29:56 +0200

    first project

---
+++ afpa.js
@@ -0,0 +1,35 @@
+// var annee = prompt("Saisir une année");
+// console.log(annee);
+/**
+ 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048, 2052, 2056, 2060, 2064, 2068, 2072, 2076, 2080, 2084, 2088, 2092, 2096.
+ */
+//  if ((annee%4 != 0 && annee%100 != 0) || (annee %400 == 0)){
+//     console.log("annee OK");
+// }
+// else{       
+//     console.log("annee KO");
+// }
+
+
+//Les boucles
+//while
+// var index = 5;
+// while(index > 0){
+//     console.log(index);
+//     //alert("j'aime le javascript");
+//     console.log("j'aime le javascript");
+//     //index++; // incrémentation
+//     //index= index +1
+//     index--; // décrémentation
+// }
+//for
+console.log("Boucle for");
+for(var index = 0; index < 5; index++){
+     console.log("j'aime le javascript");
+ }
+var index = 0;
+ do{
+     console.log("Test")
+     index ++;
+}
+while( index <5 )
